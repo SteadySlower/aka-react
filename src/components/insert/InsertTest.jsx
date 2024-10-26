b1786f524e45237b3e74b88a16f75d48577c7330
JW Moon <moon910904@gmail.com>
2024-10-26 20:32:54 +0900

    feature: add title in InsertTest

--- src/components/insert/InsertTest.jsx
+++ src/components/insert/InsertTest.jsx
@@ -3,25 +3,39 @@ import InsertQuestion from "./InsertQuestion";
 import QuestionCard from "./QuestionCard";
 
 function InsertTest() {
+    const [title, setTitle] = useState("");
     const [questions, setQuestions] = useState([]);
+    const handleChange = (e) => {
+        setTitle(e.target.value);
+    };
     const handleQuestionAdded = (question) => {
         setQuestions((questions) => [...questions, question]);
     };
     const handleOnClick = () => {
+        // TODO: validate test
         const newTest = {
-            title: "",
-            questions: questions
-        }
-        console.log(newTest)
-    }
+            title: title,
+            questions: questions,
+        };
+        console.log(newTest);
+    };
 
     return (
         <div>
             <ol>
                 {questions.map((q, i) => (
-                    <li key={i}><QuestionCard question={q} /></li>
+                    <li key={i}>
+                        <QuestionCard question={q} />
+                    </li>
                 ))}
             </ol>
+            <input
+                type="text"
+                value={title}
+                placeholder="테스트 제목"
+                required
+                onChange={handleChange}
+            />
             <button onClick={handleOnClick}>새로운 시험지 등록 완료</button>
             <InsertQuestion onQuestionAdded={handleQuestionAdded} />
         </div>
