35cb13e430b4ce56b7ea2020329b98c47c69fedf
JW Moon <moon910904@gmail.com>
2024-10-27 20:06:22 +0900

    design: add choice design

--- src/components/Test/Question.jsx
+++ src/components/Test/Question.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import { useAnswerContext } from "../../context/AnswerContext";
+import style from "./DoTest.module.scss";
+import {
+    TbCircleNumber1,
+    TbCircleNumber1Filled,
+    TbCircleNumber2,
+    TbCircleNumber2Filled,
+    TbCircleNumber3,
+    TbCircleNumber3Filled,
+    TbCircleNumber4,
+    TbCircleNumber4Filled,
+} from "react-icons/tb";
 
 function Question({ question: { id, number, instruction, passage, choices } }) {
     const { getAnswer, setAnswer } = useAnswerContext();
@@ -15,16 +26,53 @@ function Question({ question: { id, number, instruction, passage, choices } }) {
                 </h3>
                 <p>{passage}</p>
             </div>
-            <ol>
+            <ul>
                 {choices.map((choice, index) => (
-                    <li key={index} onClick={() => handleClick(index)}>
-                        {choice}
+                    <li className={getAnswer(id) === index ? style.choiceChosen : style.choice} key={index} onClick={() => handleClick(index)}>
+                        <div className={style.choice}>
+                            <Mark number={index + 1} isSelected={getAnswer(id) === index} />
+                            {choice}
+                        </div>
                     </li>
                 ))}
-            </ol>
-            {getAnswer(id) !== undefined && `선택된 답: ${getAnswer(id) + 1}`}
+            </ul>
         </section>
     );
+}
+
+function Mark({ number, isSelected }) {
+    switch (number) {
+        case 1:
+            return isSelected ? (
+                <TbCircleNumber1Filled className={style.circleChosen} />
+            ) : (
+                <TbCircleNumber1 className={style.circle} />
+            );
+        case 2:
+            return isSelected ? (
+                <TbCircleNumber2Filled className={style.circleChosen} />
+            ) : (
+                <TbCircleNumber2 className={style.circle} />
+            );
+        case 3:
+            return isSelected ? (
+                <TbCircleNumber3Filled className={style.circleChosen} />
+            ) : (
+                <TbCircleNumber3 className={style.circle} />
+            );
+        case 4:
+            return isSelected ? (
+                <TbCircleNumber4Filled className={style.circleChosen} />
+            ) : (
+                <TbCircleNumber4 className={style.circle} />
+            );
+        default:
+            return isSelected ? (
+                <TbCircleNumber1Filled className={style.circleChosen} />
+            ) : (
+                <TbCircleNumber1Filled className={style.circle} />
+            );
+    }
 }
 
 export default Question;
