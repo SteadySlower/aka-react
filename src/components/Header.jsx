409c75c2bcdc5e5f2f783f7f7a7a6ba0c3785937
JW Moon <moon910904@gmail.com>
2024-10-27 18:29:07 +0900

    feature: header home icon styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext";
+import { FiBook } from "react-icons/fi";
+import styles from "./Header.module.scss";
 
 function Header(props) {
     const { user, logout } = useAuthContext();
     return (
         <header style={{ display: "flex", justifyContent: "space-between" }}>
-            <Link to="/">
+            <Link className={styles.homeLink} to="/">
+                <FiBook className={styles.icon} />
                 <h1>AKA ENGLISH</h1>
             </Link>
             <nav
