f6a53d7b248906a94d613b9eba37c632b861b49f
JW Moon <moon910904@gmail.com>
2024-10-27 18:38:21 +0900

    feature: add Header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,21 +7,27 @@ import styles from "./Header.module.scss";
 function Header(props) {
     const { user, logout } = useAuthContext();
     return (
-        <header style={{ display: "flex", justifyContent: "space-between" }}>
+        <header className={styles.header}>
             <Link className={styles.homeLink} to="/">
                 <FiBook className={styles.icon} />
                 <h1>AKA ENGLISH</h1>
             </Link>
-            <nav
-                style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    gap: "10px",
-                }}
-            >
-                {user && <Link to="/test">Tests</Link>}
-                {user && user.isTeacher && <Link to="/insert">Add Test</Link>}
-                {user && <Link to="/result">Test Result</Link>}
+            <nav className={styles.nav}>
+                {user && (
+                    <Link to="/test" className={styles.link}>
+                        Tests
+                    </Link>
+                )}
+                {user && user.isTeacher && (
+                    <Link to="/insert" className={styles.link}>
+                        Add Test
+                    </Link>
+                )}
+                {user && !user.isTeacher && (
+                    <Link to="/result" className={styles.link}>
+                        Test Result
+                    </Link>
+                )}
                 {user && <button onClick={logout}>Logout</button>}
             </nav>
         </header>
